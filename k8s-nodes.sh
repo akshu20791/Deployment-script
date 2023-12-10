ccf0359053938aa691f5128799d8b253bf93588a
AKSHAT GUPTA <akshu20791@gmail.com>
2023-12-10 16:10:51 +0530

    Create k8s-nodes.sh

---
+++ k8s-nodes.sh
@@ -0,0 +1,26 @@
+## Install Docker
+
+
+sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installDocker.sh -P /tmp
+sudo chmod 755 /tmp/installDocker.sh
+sudo bash /tmp/installDocker.sh
+sudo systemctl restart docker.service
+
+
+## Install CRI-Docker
+
+
+sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installCRIDockerd.sh -P /tmp
+
+
+sudo chmod 755 /tmp/installCRIDockerd.sh
+sudo bash /tmp/installCRIDockerd.sh
+sudo systemctl restart cri-docker.service
+
+
+## Install kuubernetes
+
+
+sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installK8S-v1-23.sh -P /tmp
+sudo chmod 755 /tmp/installK8S-v1-23.sh
+sudo bash /tmp/installK8S-v1-23.sh
