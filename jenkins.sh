ab2663a6a6febf281f401fa8f7ea30a6c7acc40b
AKSHAT GUPTA <akshu20791@gmail.com>
2024-07-27 10:59:23 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # USE UBUNTU20.04 - INSTANCE: 2GB RAM + 2VCPU MIN - WILL ONLY WORK
 sudo apt update -y
-sudo apt install openjdk-11-jdk -y
+sudo apt install openjdk-17-jdk -y
 sudo apt update -y
 sudo apt install maven -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
