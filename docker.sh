b17a7aa15eb5df36c738e9ff788154c417c4208a
AKSHAT GUPTA <akshu20791@gmail.com>
2024-11-13 13:26:10 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,6 @@
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
